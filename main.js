a0fc255ad9f1dcab6897e79fff52573112bb0671
Stoffer06 <stofferprogramming@gmail.com>
2021-03-22 14:23:37 +0000

    22

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 var audio = document.getElementById('newRelease');
 var icon = document.getElementById('icon');
 icon.onclick = function(){
-    audio.play()
+    if(audio.paused){
+        audio.play();
+        icon.src = "img/pause.png"
+    }else{
+        audio.pause();
+        icon.src = "img/play.png"
+    }
+    
 }
